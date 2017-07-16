dde7cc02619e8863778867d9a75fa5972ee2b90d
thiagoripardo <tmripardo@gmail.com>
2017-07-16 17:04:33 -0300

    Base to monitor add

---
+++ src/monitor/Agent.java
@@ -0,0 +1,32 @@
+package monitor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Agent extends Thread{
+	
+	private int randomIngredient;
+	private Random random = new Random();
+	private String myIngredients = new String();
+	
+	@Override
+	public void run() {
+		
+		while(true) {
+			
+			this.randomIngredient = this.random.nextInt(3);
+			if (this.randomIngredient == 0) {
+				this.myIngredients = "Tabacco and Paper";
+			} 
+			else if (this.randomIngredient == 1) {
+				this.myIngredients = "Tabacco and Matches";
+			} 
+			else if (this.randomIngredient == 2) {
+				this.myIngredients = "Matches and Paper";
+			}
+			
+			
+		}
+	}
+}
